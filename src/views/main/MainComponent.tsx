3d0c0b800657cbd0b2f7aadccd4adeb449d7abc7
Alvaro Capaceta <ingcapadev@gmail.com>
2023-12-05 14:42:54 -0700

    feat(main): Handle sidebar logic

--- src/views/main/MainComponent.tsx
+++ src/views/main/MainComponent.tsx
@@ -1,14 +1,25 @@
 'use client';
+import { classNames } from '@/utils/classnames';
 import { useState } from 'react';
 
 const MainComponent = () => {
   const [showSidebar, setShowSidebar] = useState(false);
+
+  const handleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
   return (
     <div className='bg-black w-full h-screen flex'>
-      <div className='bg-blue-500 w-[28rem]'>
+      <div className={classNames(showSidebar && 'w-[28rem]', 'bg-blue-500')}>
         <div className='flex justify-end'></div>
       </div>
-      <button className='absolute bg-red-500 w-5 h-36 rounded-tr-xl rounded-br-xl top-36 left-[28rem]'></button>
+      <button
+        className={classNames(
+          showSidebar && 'left-[28rem]',
+          'absolute bg-red-500 w-5 h-36 rounded-tr-xl rounded-br-xl top-36'
+        )}
+        onClick={handleSidebar}></button>
       <div className='bg-green-500 flex-auto'></div>
     </div>
   );
