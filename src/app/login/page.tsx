75daf740b095258af38b6ca568da94a8d32f8ff5
MarcosLeva <marcoslevagomez@gmail.com>
2023-12-08 16:05:10 -0700

    login design

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default function AuthenticationPage() {
   return (
     <>
-      <div className="container relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="container relative h-screen flex-col items-center justify-center lg:grid lg:max-w-none lg:grid-cols-2 lg:px-0 bg-login-image bg-cover bg-no-repeat lg:bg-none">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-zinc-900 bg-login-image bg-cover bg-no-repeat" />
           <div className="relative z-20 flex items-center text-lg font-medium">
@@ -29,8 +29,11 @@ export default function AuthenticationPage() {
             </blockquote>
           </div>
         </div>
-        <div className="lg:p-8 flex items-center justify-center m-auto h-full">
-          <div className="m-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="lg:p-8 flex items-center justify-center m-auto h-full flex-col md py-10">
+          <Link href={"/"} className="lg:hidden">
+            <Image src={"/assets/logo.png"} width={100} height={100} alt="" />
+          </Link>
+          <div className="m-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px] bg-background p-4  lg:p-0 rounded-lg">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">
                 Iniciar sesión
@@ -40,7 +43,7 @@ export default function AuthenticationPage() {
               </p>
             </div>
             <UserAuthForm />
-            <p className="px-8 text-center text-sm text-muted-foreground">
+            <p className="px-0 lg:px-8 text-center text-sm text-muted-foreground">
               Al dar click en Continuar, estás aceptando nuestros{" "}
               <Link
                 href="/terms"
