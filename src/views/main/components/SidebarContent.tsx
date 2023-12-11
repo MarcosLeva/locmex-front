01ab1aceaa22f0889f58144be6f7131abf7ce587
Alvaro Capaceta <ingcapadev@gmail.com>
2023-12-11 09:40:12 -0700

    style(ui): Table component

--- src/views/main/components/SidebarContent.tsx
+++ src/views/main/components/SidebarContent.tsx
@@ -3,6 +3,15 @@ import { Accordion } from '@/components/ui/accordion';
 import AccordionEntry from './AccordionEntry';
 import { Compass, MapPin, ShieldQuestionIcon, Truck } from 'lucide-react';
 import { Button } from '@/components/ui/button';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
 
 const SidebarContent = () => {
   return (
@@ -12,20 +21,25 @@ const SidebarContent = () => {
       className='w-full text-white'
       defaultValue='item-1'>
       <AccordionEntry value='item-1' title='Unidades' icon={<Truck />}>
-        <div className='flex flex-col gap-2'>
-          <Button variant={'outline'} className='flex-1 rounded-none'>
-            <ShieldQuestionIcon className='w-6 h-6' />
-            <span>Unidades</span>
-          </Button>
-          <Button variant={'outline'} className='flex-1 rounded-none'>
-            <ShieldQuestionIcon className='w-6 h-6' />
-            <span>Unidades</span>
-          </Button>
-          <Button variant={'outline'} className='flex-1 rounded-none'>
-            <ShieldQuestionIcon className='w-6 h-6' />
-            <span>Unidades</span>
-          </Button>
-        </div>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className='w-[100px]'>Name</TableHead>
+              <TableHead>Invoice</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Method</TableHead>
+              <TableHead className='text-right'>Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className='font-medium'>INV001</TableCell>
+              <TableCell>Paid</TableCell>
+              <TableCell>Credit Card</TableCell>
+              <TableCell className='text-right'>$250.00</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
       </AccordionEntry>
       <AccordionEntry value='item-2' title='Geocercas' icon={<Compass />}>
         <div className='flex flex-col gap-2'>
