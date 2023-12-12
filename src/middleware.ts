d44bae4bdda518774ebc1649a22e80110b2450c5
Alvaro Capaceta <ingcapadev@gmail.com>
2023-12-11 20:39:31 -0700

    refactor(next): Nextjs cookies store

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,10 @@
+import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get('token');
+  const cookiesStore = cookies();
+  const token = cookiesStore.get('token');
   if (!token) {
     return NextResponse.redirect(new URL('/login', request.nextUrl));
   }
