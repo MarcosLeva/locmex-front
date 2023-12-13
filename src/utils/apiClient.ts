442436b55ab6510c7757ec8db9f7823b45e87ba3
Alvaro Capaceta <ingcapadev@gmail.com>
2023-12-13 06:16:55 -0700

    chore(axios): Api client

---
+++ src/utils/apiClient.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import { cookies } from 'next/headers';
+
+const cookiesStore = cookies();
+const token = cookiesStore.get('token');
+let headers = {};
+if (token) {
+  headers = { Authorization: `${token}` };
+}
+
+const apiClient = axios.create({
+  baseURL: 'http://54.214.130.15:8082/api/',
+  headers,
+});
