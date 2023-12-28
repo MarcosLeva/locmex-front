34f60c8a4122669571fe92a483894cf58fcb7b5a
Alvaro Capaceta <ingcapadev@gmail.com>
2023-12-28 09:44:02 -0700

    feat(): Axios new config for api calls

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -5,6 +5,7 @@ const token = getCookie('token');
 
 export const apiClient = axios.create({
   baseURL: 'http://54.214.130.15:8082/api/',
+  withCredentials: false,
   headers: {
     hasAuthorization: token ? true : false,
     Authorization: token?.toString(),
