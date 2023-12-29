7cb78f522009e857160dacc6f027577c569ec5bb
Alvaro Capaceta <ingcapadev@gmail.com>
2023-12-29 12:15:27 -0700

    feat(sidebar): Geofences columns for react datatable

--- src/views/main/components/SidebarContent.tsx
+++ src/views/main/components/SidebarContent.tsx
@@ -1,11 +1,16 @@
 'use client';
-import { Accordion } from '@/components/ui/accordion';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+} from '@/components/ui/accordion';
 import AccordionEntry from './AccordionEntry';
 import { Compass, Loader2, MapPin, Truck } from 'lucide-react';
 import { useColumns, type Vehiculos } from '@/views/table/components/Columns';
 import { DataTable } from '@/views/table/DataTable';
 import { useToast } from '@/components/ui/use-toast';
 import { useGeofences } from '@/services/geofencesData';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 type Props = {
   units: Vehiculos[];
@@ -20,47 +25,49 @@ const SidebarContent: React.FC<Props> = ({ units, unitsLoading }) => {
   if (error && !isLoading && !isRefetching) {
     toast({
       title: 'Error',
-      description: 'Ocurrio un error al cargar los vehiculos',
+      description: 'Ocurrio un error al cargar las geocercas',
       variant: 'destructive',
       duration: 2500,
     });
   }
 
   return (
-    <Accordion
-      type='single'
-      collapsible
-      className='w-full text-white overflow-y-auto h-[calc(100vh-7rem)]'
-      defaultValue='item-1'>
-      <AccordionEntry value='item-1' title='Unidades' icon={<Truck />}>
-        {unitsLoading ? (
-          <div className='flex justify-center items-center h-32'>
-            <Loader2 className='h-10 w-10 animate-spin' />
-          </div>
-        ) : (
-          <DataTable columns={columns} data={units} />
-        )}
-      </AccordionEntry>
-      <AccordionEntry value='item-2' title='Geocercas' icon={<Compass />}>
-        {isLoading || isRefetching ? (
-          <div className='flex justify-center items-center h-32'>
-            <Loader2 className='h-10 w-10 animate-spin' />
-          </div>
-        ) : (
-          <div className='flex flex-col gap-2'>
-            {data.map((geofence: any, index: any) => (
-              <div key={index}>{JSON.stringify(geofence)}</div>
-            ))}
-          </div>
-        )}
-      </AccordionEntry>
-      <AccordionEntry
-        value='item-3'
-        title='Puntos de interés'
-        icon={<MapPin />}>
-        <div className='flex flex-col gap-2'>asdf</div>
-      </AccordionEntry>
-    </Accordion>
+    <ScrollArea className='h-[calc(100vh-7rem)] w-full'>
+      <Accordion
+        type='single'
+        collapsible
+        className='w-full text-white'
+        defaultValue='item-1'>
+        <AccordionEntry value='item-1' title='Unidades' icon={<Truck />}>
+          {unitsLoading ? (
+            <div className='flex justify-center items-center h-32'>
+              <Loader2 className='h-10 w-10 animate-spin' />
+            </div>
+          ) : (
+            <DataTable columns={columns} data={units} />
+          )}
+        </AccordionEntry>
+        <AccordionEntry value='item-2' title='Geocercas' icon={<Compass />}>
+          {isLoading || isRefetching ? (
+            <div className='flex justify-center items-center h-32'>
+              <Loader2 className='h-10 w-10 animate-spin' />
+            </div>
+          ) : (
+            <div className='w-full flex flex-col bg-blue-300'>
+              {data.map((geofence: any, index: any) => (
+                <p key={index}>{JSON.stringify(geofence)}</p>
+              ))}
+            </div>
+          )}
+        </AccordionEntry>
+        <AccordionEntry
+          value='item-3'
+          title='Puntos de interés'
+          icon={<MapPin />}>
+          <div className='flex flex-col gap-2'>asdf</div>
+        </AccordionEntry>
+      </Accordion>
+    </ScrollArea>
   );
 };
 export default SidebarContent;
--- src/views/table/components/GeoColumns.tsx
+++ src/views/table/components/GeoColumns.tsx
@@ -1,1 +1,133 @@
 //
+'use client';
+
+import relativeTime from 'dayjs/plugin/relativeTime';
+import esmx from 'dayjs/locale/es-mx';
+import dayjs from 'dayjs';
+dayjs.extend(relativeTime);
+dayjs.locale(esmx);
+
+import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuItem,
+  DropdownMenuSub,
+  DropdownMenuSubTrigger,
+  DropdownMenuSubContent,
+} from '@/components/ui/dropdown-menu';
+import {
+  Tooltip,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+import { TooltipContent } from '@radix-ui/react-tooltip';
+import { ColumnDef } from '@tanstack/react-table';
+import {
+  BarChartHorizontal,
+  Delete,
+  Edit,
+  Flag,
+  MoreHorizontal,
+  Share,
+  User,
+} from 'lucide-react';
+import { useSelectedRows } from '@/stores/selectedRows';
+
+export type Geocercas = {
+  IdZona: string;
+  IdTipoZona: string;
+  IdExterno: string;
+  Descripcion: string;
+  Filtro: boolean;
+  Inclusion: boolean;
+  Activo: boolean;
+  IsMarked: boolean;
+  Latitud: number;
+  Longitud: number;
+  IdUsuarioModificacion: string;
+  FechaModificacion: string;
+};
+
+export const useColumns = () => {
+  // const handleSelectedRows = useSelectedRows(
+  //   (state) => state.handleSelectedRows
+  // );
+  // const rows = useSelectedRows((state) => state.rows);
+  // const isAllSelected = useSelectedRows((state) => state.isAllSelected);
+  // const isSomeSelected = useSelectedRows((state) => state.isSomeSelected);
+  // const toggleAllSelected = useSelectedRows((state) => state.toggleAllSelected);
+
+  const columns: ColumnDef<Geocercas>[] = [
+    {
+      id: 'select',
+      header: ({ table }) => (
+        <Checkbox
+          // checked={isAllSelected() || (isSomeSelected() && 'indeterminate')}
+          // onCheckedChange={toggleAllSelected}
+          aria-label='Select all'
+        />
+      ),
+      cell: ({ row }) => {
+        // const index = rows.findIndex(
+        //   (selectedRow) => selectedRow.id === row.original.IdZona
+        // );
+        // const isSelected = index !== -1 ? rows[index].selected : false;
+
+        return (
+          <Checkbox
+            // checked={isSelected}
+            // onCheckedChange={(value) => {
+            //   handleSelectedRows(row.original.IdZona);
+            // }}
+            aria-label='Select row'
+          />
+        );
+      },
+      enableSorting: false,
+      enableHiding: false,
+    },
+    {
+      accessorKey: 'Descripcion',
+      header: 'Descripción',
+      cell: ({ row }) => (
+        <div className='max-w-[70px] truncate'>
+          <span>{row.original.Descripcion}</span>
+        </div>
+      ),
+    },
+    {
+      id: 'actions',
+      cell: ({ row }) => (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant='ghost'
+              className='flex h-8 w-8 p-0 data-[state=open]:bg-muted'>
+              <MoreHorizontal />
+              <span className='sr-only'>Open menu</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align='end' className='w-[160px]'>
+            <DropdownMenuItem>
+              <div className='flex gap-3'>
+                <Edit />
+                Editar
+              </div>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <div className='flex gap-3'>
+                <Delete />
+                Eliminar
+              </div>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      ),
+    },
+  ];
+  return { columns };
+};
+export default useColumns;
