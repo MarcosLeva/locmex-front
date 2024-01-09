cb6b27fd1b18c62baa25707ac2eae75e17ff0559
Alvaro Capaceta <ingcapadev@gmail.com>
2024-01-09 00:52:24 -0700

    feat(services): Geofences points get data and custom hook query

---
+++ src/services/geofencesPointsData.ts
@@ -0,0 +1,18 @@
+import { apiClient } from '@/utils/apiClient';
+import { useQuery } from '@tanstack/react-query';
+
+export const getGeofencesPoints = async () => {
+  try {
+    const { data } = await apiClient.post('zonapunto/get', {});
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const useGeofencesPoints = () => {
+  return useQuery({
+    queryKey: ['geocercasPuntos'],
+    queryFn: getGeofencesPoints,
+  });
+};
