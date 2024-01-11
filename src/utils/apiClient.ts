009d1b643064a6dd99f260de5020e44c5aeac47f
MarcosLeva <marcoslevagomez@gmail.com>
2024-01-10 20:59:34 -0700

    deploy

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -4,7 +4,7 @@ import { getCookie } from "cookies-next";
 const token = getCookie("token");
 
 export const apiClient = axios.create({
-  baseURL: "http://54.214.130.15:8082/api/",
+  baseURL: "https://54.214.130.15:8082/api/",
   withCredentials: false,
   headers: {
     hasAuthorization: token ? true : false,
